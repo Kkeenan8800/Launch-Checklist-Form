7a565ac891a3d56f353c95a6eefdd3fa77bc3d34
unknown <kkeenan8800@gmail.com>
2019-10-14 21:33:34 -0500

    added form validation, checks if input exists and if it's the right type

--- script.js
+++ script.js
@@ -1,5 +1,31 @@
 // Write your JavaScript code here!
 
+window.addEventListener("load", function(){
+   let button = document.getElementById("formSubmit");
+   button.addEventListener("click", function(event){
+      let pilotName = document.querySelector("input[name=pilotName]");
+      let copilotName = document.querySelector("input[name=copilotName]");
+      let fuelLevel = document.querySelector("input[name=fuelLevel]");
+      let cargoMass = document.querySelector("input[name=cargoMass]");
+      
+      //Validates Form
+      if(pilotName.value === '' || copilotName.value === '' || fuelLevel.value === '' || cargoMass.value === ''){
+         alert("All fields are required");
+         event.preventDefault();
+      }else if(!isNaN(pilotName.value) || !isNaN(copilotName.value) || isNaN(fuelLevel.value) || isNaN(cargoMass.value)){
+         alert('invalid input');
+         event.preventDefault();
+      }
+
+      //Updating Info 
+      let div = document.getElementById("faultyItems")
+      div.innerHTML = ''
+
+      
+   })
+})
+
+
 /* This block of code shows how to format the HTML once you fetch some planetary JSON!
 <h2>Mission Destination</h2>
 <ol>
