04c025e0776cb5316e810948c2dd4136711bfd49
unknown <kkeenan8800@gmail.com>
2019-10-20 20:41:23 -0500

    Update index.html

--- script.js
+++ script.js
@@ -3,25 +3,64 @@
 window.addEventListener("load", function(){
    let button = document.getElementById("formSubmit");
    button.addEventListener("click", function(event){
+      event.preventDefault();
+
       let pilotName = document.querySelector("input[name=pilotName]");
       let copilotName = document.querySelector("input[name=copilotName]");
       let fuelLevel = document.querySelector("input[name=fuelLevel]");
       let cargoMass = document.querySelector("input[name=cargoMass]");
       
-      //Validates Form
+   //Validating Form 
       if(pilotName.value === '' || copilotName.value === '' || fuelLevel.value === '' || cargoMass.value === ''){
          alert("All fields are required");
-         event.preventDefault();
       }else if(!isNaN(pilotName.value) || !isNaN(copilotName.value) || isNaN(fuelLevel.value) || isNaN(cargoMass.value)){
          alert('invalid input');
-         event.preventDefault();
+      }else{ //form validated:
+
+      //Checking Fuel and Cargo Mass
+         let fuelLevelStatus = '';
+         let fuelReady;
+         let cargoMassStatus = '';
+         let cargoReady;
+
+         if(fuelLevel.value < 10000){ 
+            fuelLevelStatus = "Fuel Level is too Low";
+            fuelReady = false
+         }else{ 
+            fuelLevelStatus =`Fuel Level: ${fuelLevel.value}`;
+            fuelReady = true
+         }
+
+         if(cargoMass > 10000){
+            cargoMassStatus = "Cargo is too heavy";
+            cargoReady = false
+         }else{
+            cargoMassStatus = `Cargo Mass: ${cargoMass.value}`;
+            cargoReady = true 
+         }
+
+      //Updating Launch Checklist Items
+         let faultyItems = document.getElementById("faultyItems");
+         faultyItems.innerHTML = `
+            <ol>
+               <li id="pilotStatus">Pilot ${pilotName.value} Ready</li>
+               <li id="copilotStatus">Co-pilot ${copilotName.value} Ready</li>
+               <li id="fuelStatus">${fuelLevelStatus}</li>
+               <li id="cargoStatus">${cargoMassStatus}</li> 
+            </ol>
+         `
+         faultyItems.style.visibility = 'visible'
+
+      //Updating launch Checklist Heading 
+         let launchStatusHeading = document.getElementById("launchStatus");
+         if(fuelReady && cargoReady){
+            launchStatusHeading.innerHTML = `Shuttle is ready for launch.`
+            launchStatusHeading.style.color = 'green'
+         }else{
+            launchStatusHeading.innerHTML = `Shuttle is not ready for launch.`
+            launchStatusHeading.style.color = "red"
+         }
       }
-
-      //Updating Info 
-      let div = document.getElementById("faultyItems")
-      div.innerHTML = ''
-
-      
    })
 })
 
